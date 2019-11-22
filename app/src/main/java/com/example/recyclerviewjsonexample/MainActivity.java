3fa4778a7855b0d85c7b93c381586f27178986e2
rutaks <samrutakayile@awesomity.rw>
2019-11-22 04:57:00 +0200

    Initial Setup

---
+++ app/src/main/java/com/example/recyclerviewjsonexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.recyclerviewjsonexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
